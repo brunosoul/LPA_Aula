46f5e064e973ee1a0d304ea678053485fdc1c0e6
Bruno Faria <brunosouzafaria@id.uff.br>
2023-10-16 22:47:56 -0300

    Signed-off-by: Bruno Faria <brunosouzafaria@id.uff.br>

---
+++ Model/Entity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Entity:
+    def __init__(self):
+        self.Name = None
+        self.image = None
+        self.rect = None
+
+    def Move(self, ):
+        pass
+
---
+++ Model/Level.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.window = None
+        self.Entity_list = None
+        self.Name = None
+
+    def run(self, ):
+        pass
+
+    def Operation1(self, ):
+        pass
+
